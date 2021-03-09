a66a0ed66c44d9b2373b9c86f1ce5858774a39a1
Ronak Panchal <ronubca@gmail.com>
2021-03-09 15:21:50 +0530

    First Heroku commit

--- bananait/bananait/settings.py
+++ bananait/bananait/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = BASE_DIR / 'static'
+#STATICFILES_DIRS = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
