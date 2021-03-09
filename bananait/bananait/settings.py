923c253a86bd1f0b3721fc7052c8eb4eb421b736
Ronak Panchal <ronubca@gmail.com>
2021-03-09 15:03:27 +0530

    First Heroku commit

--- bananait/bananait/settings.py
+++ bananait/bananait/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'bananait.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
